0e61dd938197178ebfa6a6efb4e454c162dfa148
Chriszkxxx <kaixinzhu56@gmail.com>
2025-12-31 11:33:27 +0800

    fix

--- test/test_flash_world.py
+++ test/test_flash_world.py
@@ -1,20 +1,19 @@
 import sys
 sys.path.append("..")
 
-from src.sceneflow.pipelines.flash_world.pipeline_flash_world import FlashWorldPipeline
+from sceneflow.pipelines.flash_world.pipeline_flash_world import FlashWorldPipeline
 
 # Configure before running
 MODEL_PATH = "imlixinyang/FlashWorld"
-JSON_CONFIG_PATH = "/data/zhukaixin/FlashWorld/examples" #None  # Path to JSON config file, or None to use defaults
 
 # Offload options (reduce GPU memory usage)
 OFFLOAD_T5 = True  # Offload text encoder to CPU
-OFFLOAD_VAE = True  # Offload VAE to CPU (greatly reduces GPU memory but increases generation time)
+OFFLOAD_VAE = False  # Offload VAE to CPU (greatly reduces GPU memory but increases generation time)
 OFFLOAD_TRANSFORMER_DURING_VAE = True  # Offload transformer during VAE processing
 
-# Default configuration (used when JSON_CONFIG_PATH is None)
-TEXT_PROMPT = ""
-IMAGE_PATH = None
+# Default configuration 
+TEXT_PROMPT = "A cozy medieval-style village square on a winter evening, with timber-framed cottages"
+IMAGE_PATH = "../data/test_case1/ref_image.png"
 NUM_FRAMES = 16
 IMAGE_HEIGHT = 480
 IMAGE_WIDTH = 704
@@ -30,20 +29,17 @@ pipeline = FlashWorldPipeline.from_pretrained(
     offload_transformer_during_vae=OFFLOAD_TRANSFORMER_DURING_VAE,
 )
 
-if JSON_CONFIG_PATH is not None:
-    config = FlashWorldPipeline.load_config_from_json(JSON_CONFIG_PATH)
-    results = pipeline(**config)
-else:
-    results = pipeline(
-        input_=IMAGE_PATH,
-        text_prompt=TEXT_PROMPT,
-        num_frames=NUM_FRAMES,
-        image_height=IMAGE_HEIGHT,
-        image_width=IMAGE_WIDTH,
-        image_index=IMAGE_INDEX,
-        return_video=RETURN_VIDEO,
-        video_fps=VIDEO_FPS,
-    )
+
+results = pipeline(
+    input_=IMAGE_PATH,
+    text_prompt=TEXT_PROMPT,
+    num_frames=NUM_FRAMES,
+    image_height=IMAGE_HEIGHT,
+    image_width=IMAGE_WIDTH,
+    image_index=IMAGE_INDEX,
+    return_video=RETURN_VIDEO,
+    video_fps=VIDEO_FPS,
+)
 
 pipeline.save_results(results=results, output_dir=OUTPUT_DIR)
 
